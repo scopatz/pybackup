b6d29210463b50115cd597df40a3bda3fdc9ce54
Anthony Scopatz <scopatz@gmail.com>
2011-01-31 12:48:04 -0600

    Some more minor changes to rsync_dir

--- src/rsync_dir.py
+++ src/rsync_dir.py
@@ -26,11 +26,15 @@ class RsyncDir(object):
 
         try:
             if self.deleteflag:
-                subprocess.call("rsync --stats --progress --partial -h -avrlLH --delete-after --rsh=ssh {localdir} {remoteuser}@{remotehost}:{remotedir}".format(**self.__dict__), shell=True)
+                cmd = "rsync --stats --progress --partial -h --rsh=ssh -avrlLH --delete-after {localdir} {remoteuser}@{remotehost}:{remotedir}".format(**self.__dict__)
             else:
-                subprocess.call("rsync --stats --progress --partial -h -avrlLH --rsh=ssh {localdir} {remoteuser}@{remotehost}:{remotedir}".format(**self.__dict__), shell=True)
+                cmd = "rsync --stats --progress --partial -h --rsh=ssh -avrlLH {localdir} {remoteuser}@{remotehost}:{remotedir}".format(**self.__dict__)
+
+            subprocess.call(cmd, shell=True)
             t2 = time.time()
+
             print("\033[1;32m{0:{1}}...Completed in \033[1;33m{2:.3G}\033[1;32m min!\033[0m".format("", len(startstr)-3-28, (t2-t1)/60.0) )
+
         except:
             t2 = time.time()
             print("\033[1;31m{0:{1}}...Failed after \033[1;33m{2:.3G}\033[1;31m min!\033[0m".format("", len(startstr)-3-28, (t2-t1)/60.0) )
