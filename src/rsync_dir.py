0bd69ade5cb86f64bee41af6d336e2e01396fb9e
Anthony Scopatz <scopatz@gmail.com>
2011-01-31 12:36:24 -0600

    Some minor changes to rsync_dir

--- src/rsync_dir.py
+++ src/rsync_dir.py
@@ -26,9 +26,9 @@ class RsyncDir(object):
 
         try:
             if self.deleteflag:
-                subprocess.call("rsync --stats --progress --partial -h -avrlLH --delete-after -e ssh {localdir} {remoteuser}@{remotehost}:{remotedir}".format(**self.__dict__), shell=True)
+                subprocess.call("rsync --stats --progress --partial -h -avrlLH --delete-after --rsh=ssh {localdir} {remoteuser}@{remotehost}:{remotedir}".format(**self.__dict__), shell=True)
             else:
-                subprocess.call("rsync --stats --progress --partial -h -avrlLH -e ssh {localdir} {remoteuser}@{remotehost}:{remotedir}".format(**self.__dict__), shell=True)
+                subprocess.call("rsync --stats --progress --partial -h -avrlLH --rsh=ssh {localdir} {remoteuser}@{remotehost}:{remotedir}".format(**self.__dict__), shell=True)
             t2 = time.time()
             print("\033[1;32m{0:{1}}...Completed in \033[1;33m{2:.3G}\033[1;32m min!\033[0m".format("", len(startstr)-3-28, (t2-t1)/60.0) )
         except:
